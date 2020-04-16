9ed72fdf2f1219d3b401af8d6b8d038eeb66fe40
nuitmod <wtvfqm@gmail.com>
2020-04-16 19:04:18 +0500

    corr

--- lua_i/www/sw.js
+++ lua_i/www/sw.js
@@ -2,7 +2,16 @@ var CACHE_NAME = 'version-1'; // bump this version when you make changes.
 // Put all your urls that you want to cache in this array
 var urlsToCache = [
     'index.html',
-    'assets/logo-192.png'
+    'page1.html',
+    'page2.html',
+    'assets/logo-192.png',
+    'sw.js',
+    '../css/main.css',
+    '../css/style.css',
+    '../img/mode.jpg',
+    '../img/code.jpg',
+    '../img/mod2.jpg',
+    '../js/main.js'
 ];
 
 // Install the service worker and open the cache and add files mentioned in array to cache
@@ -44,4 +53,3 @@ self.addEventListener('activate', function(event) {
         })
     );
 });
-
